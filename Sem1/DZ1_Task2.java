52737e136dda10b989a7eea0aff81fc6e98b1c42
Artur <arturnl@ya.ru>
2022-06-07 20:21:33 +0300

    modify Task2

--- Sem1/DZ1_Task2.java
+++ Sem1/DZ1_Task2.java
@@ -1,15 +1,26 @@
+/*Попробовать сгенерировать все перестановки длины N из K чисел с повторениями. 
+Пример: N = 2, K = 3 ответ "00 01 02 10 11 12 20 21 22"*/
+
 package Sem1;
+
+import java.util.Arrays;
 
 public class DZ1_Task2 {
     public static void main(String[] args) {
-        prob(3, 2);
+        int n = 3;
+        int k = 3;
+        int[] array = new int[n];
+        prob(k, array, 0);
     }
-    public static void prob(int k, int n) {
-        for (int i = 0; i < k ; i++) {
-            for (int j = 0; j < k; j++) {
-                System.out.format("%d, %d", j, i);
-            }
+
+    public static void prob(int k, int[] arr, int index) {
+        if (index == arr.length) {
+            System.out.println(Arrays.toString(arr));
+            return;
+        }
+        for (int i = 0; i < k; i++) {
+            arr[index] = i;
+            prob(k, arr, index + 1);
         }
     }
 }
-
