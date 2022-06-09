597eeb7272336b44af096339f965d9fefae054fd
Artur <arturnl@ya.ru>
2022-06-09 20:58:45 +0300

    Les1 Task2

---
+++ Sem2/Les1_Task2.java
@@ -0,0 +1,29 @@
+//Напишите метод, который сжимает строку. Пример: вход aaaabbbcdd, результат - a4b3cd2
+
+package Sem2;
+
+public class Les1_Task2 {
+    public static void main(String[] args) {
+        String s = "aaaabbbcdd";
+        System.out.println(compressStr(s));
+    }
+
+    public static String compressStr(String str) {
+        int count = 1;
+        char t = str.charAt(0);
+        String res = "";
+        for (int i = 1; i < str.length(); i++) {
+            if (t != str.charAt(i)) {
+                res = res + t;
+                t = str.charAt(i);
+                if (count != 1) {
+                    res = res + count;
+                }
+                count = 0;
+            }
+            count++;
+        }
+        res = res + t + count;
+        return res;
+    }
+}
