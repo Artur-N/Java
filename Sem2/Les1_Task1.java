8e19162e1943c5123db76b4ec5e5cffb39d9082f
Artur <arturnl@ya.ru>
2022-06-09 20:57:15 +0300

    Correct

--- Sem2/Les1_Task1.java
+++ Sem2/Les1_Task1.java
@@ -5,17 +5,16 @@ package Sem2;
 
 public class Les1_Task1 {
     public static void main(String[] args) {
-        String s = "ababaabbaa";
+        String s = "abababa";
         System.out.println(palindrom(s));
-    }    
+    }
+
     public static boolean palindrom(String str) {
-        int mid = str.length()/2;
+        int mid = str.length() / 2;
         for (int i = 0; i < mid; i++) {
-            System.out.println(str.charAt(i)); 
-            System.out.println(str.charAt(str.length()-i-1));
-            if (str.charAt(i) != str.charAt(str.length()-i-1)) return false;
+            if (str.charAt(i) != str.charAt(str.length() - i - 1))
+                return false;
         }
         return true;
     }
 }
-
